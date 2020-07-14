df86ea002b30a123406302d286e3a7509e5bc2b4
Inioluwa Sogelola <inioluwwa4.is@gmail.com>
2020-07-14 23:09:54 +0100

    demo changes

--- examples/demo/src/login.tsx
+++ examples/demo/src/login.tsx
@@ -1,10 +1,29 @@
-import React from "react"
-import { useLogin, useLogout } from "rap-core"
-import { Button } from "rap-ui"
+import React, { useState, useEffect } from "react"
+import { useLogin, useLogout, useNotification } from "rap-core"
+import { Button, Notification } from "rap-ui"
+import { NotifyProps } from "rap-ui/lib/types"
+
+const NotificationComponent: React.FC<any> = ({ item, idx, onFinish }) => {
+  useEffect(() => {
+    (function () {
+      setTimeout(() => {
+        onFinish(idx)
+      }, 2000)
+    })()
+  }, [onFinish])
+
+  return (
+    <div>
+      <p>{item.title}</p>
+      <p>{item.text}</p>
+    </div>
+  )
+}
 
 const Login: React.FC<any> = () => {
   const login = useLogin()
   const logout = useLogout()
+  const [notification, setNotification] = useNotification<NotifyProps>()
 
   const handleLogin: any = (e: EventListener) => {
     login({ username: "log" }, "/")
@@ -12,28 +31,47 @@ const Login: React.FC<any> = () => {
   const handleLogout: any = (e: EventListener) => {
     console.log(logout({ username: "dd" }, "/"))
   }
+
   return (
     <>
       <br />
-      <Button
-        id="name"
-        background="primary"
-        glow
-        gradient
-        size={"lg"}
-      >
+      <Button id="name" background="primary" glow gradient size={"lg"}>
         Search
       </Button>
+      {notification.map((item: NotifyProps, idx: number) => (
+        <NotificationComponent
+          key={idx}
+          item={item}
+          idx={idx}
+          onFinish={(id: number) => {
+            const notificationQueueCopy = [...notification]
+            notificationQueueCopy.splice(id, 1)
+            setNotification(notificationQueueCopy)
+          }}
+        />
+      ))}
+
+      <br />
       <br />
       <Button
         id="name"
         background="info"
         glow
         gradient
-        size={"md"}
-        onClick={handleLogin}
+        size={"sm"}
+        onClick={(e: any) => {
+          // setTest([...test, "q"])
+          setNotification([
+            ...notification,
+            { title: "title", text: "this is a message" },
+          ])
+          console.log([
+            ...notification,
+            { title: "title", text: "this is a message" },
+          ])
+        }}
       >
-        Login
+        Add notification
       </Button>
     </>
   )
---
+++ packages/rap-core/src/context/index.ts
@@ -0,0 +1,7 @@
+import {
+  NotificationProvider,
+  useNotification,
+  NotificationContext,
+} from "./notification"
+
+export { useNotification, NotificationContext, NotificationProvider }
