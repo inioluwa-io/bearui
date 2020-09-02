de59e73f8bdd8014e2640222b00f4b6357515be2
Inioluwa Sogelola <inioluwwa4.is@gmail.com>
2020-09-02 18:21:06 +0100

    update demo

--- examples/demo/src/login.tsx
+++ examples/demo/src/login.tsx
@@ -29,7 +29,6 @@ const Login: React.FC<any> = () => {
   const [login, setLogin] = useState<boolean>(false)
 
   const { data: template, loading } = queryStore.getOne("template", {
-    id:"2298394i",
     name: "Plain Blue",
   })
 
