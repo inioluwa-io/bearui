7741d15fd6f4ba4629e1ffd568bf48c62fa44fa7
nerd-caveman <inioluwwa4.is@gmail.com>
2021-02-08 11:18:56 +0100

    fix: cannot find module './useHideSideBar'

--- packages/rap-ui/src/theme/index.ts
+++ packages/rap-ui/src/theme/index.ts
@@ -2,7 +2,7 @@ import ThemeProvider from "./themeContext"
 import useTheme from "./useTheme"
 import ThemeModeProvider from "./modeContext"
 import useThemeMode from "./useThemeMode"
-import useHideSideBar from "./useHideSideBar"
+import useHideSideBar from "./useHideSidear"
 import useCollapseSideBar from "./useCollapseSideBar"
 
 export {
